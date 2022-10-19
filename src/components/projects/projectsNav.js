c893d264299dc550f5edf8873d452b73ed277d4e
Max Hargreave <max@monsun.dk>
2022-10-19 17:02:53 +0200

    all btn

--- src/components/projects/projectsNav.js
+++ src/components/projects/projectsNav.js
@@ -21,7 +21,14 @@ let projectsNav = function() {
             .then((response) => response.json())
             .then((entries) => {
 
-                
+                let item = document.createElement('li')
+                item.setAttribute("id", 'All' )
+                items.append(item)
+
+                item.innerHTML = `
+                        <a href="">All ()</a>
+                    
+                    `
 
                 entries.data.forEach(entry => {
                     let item = document.createElement('li')
